f0df2140745305bc7fd5b0b58b823afed880289e
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-02-22 16:11:10 -0600

    Improve autounattend-postinstall
    - Log more debugging information by default
    - Disable vbox additions (for now, til we can figure out how to detect
      the hypervisor)
    - Use new syntax from postinstall module

--- scripts/autounattend-postinstall.ps1
+++ scripts/autounattend-postinstall.ps1
@@ -5,13 +5,18 @@
 import-module $PSScriptRoot\wintriallab-postinstall.psm1
 $errorActionPreference = "Stop"
 
-# Useful for debugging
-$message = "Environment variables: `r`n`r`n"
-foreach ($var in (Get-ChildItem env:\)) {
-    $message += "$($var.Name) = $($var.Value)`r`n"
+# Messages that aid debugging go here. Keep them extremely simple and therefore extremely unlikely to fail
+Invoke-ScriptblockAndCatch {
+    $message = "Debugging information: `r`n`r`n"
+    $message += "Running as administrator: $(Test-AdminPrivileges)`r`n`r`n"
+    $message += "Variables:`r`n"
+    foreach ($var in (Get-ChildItem env:\)) {
+        $message += "$($var.Name) = $($var.Value)`r`n"
+    }
+    Write-EventLogWrapper -message $message
 }
-Write-EventLogWrapper -message $message
 
+# Operations changing the state of the VM and other things that might be more inclined to fail go here
 Invoke-ScriptblockAndCatch -scriptBlock {
     Write-EventLogWrapper "Starting the autounattend postinstall script"
     Set-IdleDisplayPoweroffTime -seconds 0
@@ -31,17 +36,17 @@ Invoke-ScriptblockAndCatch -scriptBlock {
     # else {
     #     Write-EventLogWrapper -message "A builder called '$env:PACKER_BUILDER_TYPE' was detected, but we don't have a way to install its drivers yet"
     # }
-    Install-VBoxAdditions -fromDisc
+    # Install-VBoxAdditions -fromDisc
     
     # Required for Windows 10, not required for 81, not sure about other OSes
     # Should probably happen after installing Guest VM drivers, in case installing the drivers would cause Windows to see the network as a new connection
     Set-AllNetworksToPrivate
 
     if ($SkipWindowsUpdates) {
-        $restartCommand = [ScriptBlock]::Create("A:\enable-winrm.ps1")
+        $restartCommand = "A:\enable-winrm.ps1"
     }
     else {
-        $restartCommand = [ScriptBlock]::Create("A:\win-updates.ps1 -PostUpdateExpression A:\enable-winrm.ps1")
+        $restartCommand = "A:\win-updates.ps1 -PostUpdateExpression A:\enable-winrm.ps1"
     }
 
     Set-RestartScheduledTask -RestartCommand $restartCommand | out-null
