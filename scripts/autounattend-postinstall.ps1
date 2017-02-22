d9b7ab950d3d55064e3cd92cf8eb82b1e7e6945c
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-02-22 16:48:54 -0600

    Move Set-AllNetworksToPrivate from autounattend-postinstall to enable-winrm

--- scripts/autounattend-postinstall.ps1
+++ scripts/autounattend-postinstall.ps1
@@ -38,10 +38,6 @@ Invoke-ScriptblockAndCatch -scriptBlock {
     # }
     # Install-VBoxAdditions -fromDisc
     
-    # Required for Windows 10, not required for 81, not sure about other OSes
-    # Should probably happen after installing Guest VM drivers, in case installing the drivers would cause Windows to see the network as a new connection
-    Set-AllNetworksToPrivate
-
     if ($SkipWindowsUpdates) {
         $restartCommand = "A:\enable-winrm.ps1"
     }
--- scripts/enable-winrm.ps1
+++ scripts/enable-winrm.ps1
@@ -1,5 +1,10 @@
 import-module $PSScriptRoot\wintriallab-postinstall.psm1
 $errorActionPreference = "Stop"
 Invoke-ScriptblockAndCatch -scriptBlock {
+
+    # Required for Windows 10, not required for 81, not sure about other OSes
+    # Should probably happen after installing Guest VM drivers, in case installing the drivers would cause Windows to see the network as a new connection
+    Set-AllNetworksToPrivate
+
     Enable-WinRM
 }
