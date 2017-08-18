aa805ebfe036a68380a035b47d1ba29b6604c5b2
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-08-18 18:59:41 -0500

    Use a config file, and add other actions
    All I wanted to do was add the connect action, but the argument parser
    got too unwieldy so I rewrote it.
    
    - Add support for a config file that can be overriden by commandline
      arguments
    - Better split options into various parsers and subparsers
    - Add the 'testgroup' action to test whether the group exists
    - Add the 'connect' action to connect to the cloud builder VM over RDP
      (only partially complete) - intended to be run on the HOST machine,
      not inside the Docker container
    - Add a --showconfig flag, so you can inspect the configuration without
      deploying anything
    - Make builderVmAdminUsername a parameter, rather than a variable

--- azure/deploy.py
+++ azure/deploy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+import configparser
+import datetime
 import json
 import logging
 import os
@@ -9,13 +11,14 @@ import secrets
 import string
 import subprocess
 import sys
+import textwrap
 import urllib.request
 
 import yaml
 
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.resource import ResourceManagementClient
-
+from msrestazure.azure_exceptions import CloudError
 
 scriptdir = os.path.dirname(os.path.realpath(__file__))
 
@@ -58,6 +61,13 @@ def resolvepath(path):
     return os.path.realpath(os.path.normpath(os.path.expanduser(path)))
 
 
+def exresolvepath(path):
+    rpath = resolvepath(path)
+    if not os.path.exists(rpath):
+        raise Exception(f"Path does not exist: '{rpath}'")
+    return rpath
+
+
 def genpass(length=24):
     """Generate a passphrase that will meet default Windows complexity reqs
 
@@ -82,6 +92,15 @@ def genpass(length=24):
             return password
 
 
+def homedir():
+    for varname in ["HOME", "USERPROFILE"]:
+        home = os.environ.get(varname)
+        if home:
+            log.info(f"Found homedir '{home}' from {varname} environment variable")
+        return home
+    raise Exception("Could not determine home directory - try setting a $HOME or %USERPROFILE% variable")
+
+
 def tname2tid(name):
     """Convert a tenant name to a tenant ID
 
@@ -104,6 +123,7 @@ def deploytempl(
         groupname,
         grouplocation,
         storageacct,
+        vmadminuser,
         vmadminpass,
         vmsize,
         template,
@@ -136,6 +156,7 @@ def deploytempl(
         'template': template,
         'parameters': templparam({
             'storageAccountName': storageacct,
+            'builderVmAdminUsername': vmadminuser,
             'builderVmAdminPassword': vmadminpass,
             'builderVmSize': vmsize})
     }
@@ -157,78 +178,248 @@ def rdp_win(server, username, password):
         subprocess.check_output(['cmdkey.exe', f'/delete:TERMSRV/{server}'])
 
 
-def parseargs(*args, **kwargs):
-    parser = argparse.ArgumentParser()
+class ProcessedDeployConfig:
+    """A class that can parse arguments and read from a config file
 
-    parser.add_argument(
-        '--debug', '-d', action='store_true',
-        help="Show debug messages")
+    Set a property on the object for each item in the dictionary
+    """
 
-    templateopts = argparse.ArgumentParser(add_help=False)
-    templateopts.add_argument(
-        '--arm-template', type=resolvepath,
-        default=os.path.join(scriptdir, 'cloudbuilder.yaml'),
-        help="Location of the ARM template")
+    mastercfg = os.path.join(scriptdir, 'cloudbuilder.cfg')
+    usercfg = os.path.join(homedir(), '.wintriallab.cloudbuilder.cfg')
+    defaulttempl = os.path.join(scriptdir, 'cloudbuilder.yaml')
 
-    azureopts = argparse.ArgumentParser(add_help=False)
-    azureopts.add_argument(
-        '--deployment-name', default='wintriallab',
-        help="Deployment name")
-    azureopts.add_argument(
-        '--group-name', default="wintriallab",
-        help="Azure Resource Group name")
-    azureopts.add_argument(
-        '--group-location', default='westus2',
-        help="The resource group location. Note that only a few resource groups support the types of VMs we require; see also https://azure.microsoft.com/en-us/blog/introducing-the-new-dv3-and-ev3-vm-sizes/")
-    azureopts.add_argument(
-        '--storage-account-name', required=True,
-        help="A name for a new storage account. Note that this must follow Microsoft's rules for DNS hostnames, which are more restrictive than the DNS spec.")
-    azureopts.add_argument(
-        '--service-principal-id', required=True,
-        help="The ID (GUID) of the service princiapl to use")
-    azureopts.add_argument(
-        '--service-principal-key', required=True,
-        help="The secret key for the service principal to use")
-    azureopts.add_argument(
-        '--tenant', required=True,
-        help="The Azure Active Directory tenant (e.g. example.onmicrosoft.com)")
-    azureopts.add_argument(
-        '--subscription-id', required=True,
-        help="The Azure subscription ID (GUID)")
-    azureopts.add_argument(
-        '--builder-vm-admin-password', default=genpass(),
-        help="The admin password for the builder VM")
-    azureopts.add_argument(
-        '--builder-vm-size', default='Standard_D2_v3',
-        help="The size of the builder VM. Note that only Standard Dv3 and Standard Ev3 VMs support the nested virtualization that we need for the cloud boilder.")
+    def __init__(self, *args, **kwargs):
 
-    subparsers = parser.add_subparsers(dest="action")
-    subparsers.add_parser(
-        'convertyaml', parents=[templateopts],
-        help='Convert the YAML template to JSON')
-    subparsers.add_parser(
-        'deploy', parents=[templateopts, azureopts],
-        help='Deploy the ARM template to Azure')
-    subparsers.add_parser(
-        'delete', parents=[templateopts, azureopts],
-        help='Delete an Azure Resource Group')
+        parsed = self.parseargs(*args, **kwargs)
 
-    parsed = parser.parse_args()
-    return parsed
+        # Weed out None values from the parsed arguments
+        # I can't use (action='store_true') in my arg parser, because if the
+        # option isn't passed, it is set to False. Since we override config file
+        # parameters with options passed on the command line, this means that
+        # 'store_true' options will override a True setting in the config file
+        # if they aren't passed explicitly. So, instead I use
+        # (action='store_const', const=True), which results in None for
+        # unpassed arguments. All I do here is remove those None options.
+        parseddict = {k: v for k, v in parsed.__dict__.items() if v is not None}
+
+        # Grab configuration from default config files as well as the one passed
+        # on the commandline (if present)
+        configdict = self.parseconfig([parsed.configfile])
+
+        # Combine the configs
+        combinedconfig = {}
+        combinedconfig.update(configdict)
+        combinedconfig.update(parseddict)
+
+        # Set the attributes of this object with the combined dict
+        # This lets me do 'cfg = ProcessedDeployConfig(); print(cfg.opt_name)'
+        for k, v in combinedconfig.items():
+            setattr(self, k, v)
+
+        # Apply the defaults and check for the required parameters.
+        # I cannot do this in .add_argument(), since parameters may come in from
+        # a config file instead.
+        self.apply_runtime_defaults()
+        self.check_required_params()
+
+    def parseargs(self, *args, **kwargs):
+
+        epilog = textwrap.dedent(f"""
+            NOTES:
+
+            1.  Many arguments are required, but any of them may come from a config file.
+
+            2.  All arguments are represented in the config file with dashes replaced by
+                underscores; for instance, the --builder-vm-admin-password argument
+                corresponds to the builder_vm_admin_password entry in the config file.
+
+            3.  See the config file in the same directory as this script for all options
+                and a few notes
+
+            4.  Config files are read in this order:
+
+                1.  {self.mastercfg}
+                2.  {self.usercfg}
+                3.  Any config file passed on the command line with --config
+
+                Any value in a later, existing config file will overwrite values from
+                earlier config files.
+            """)
+
+        parser = argparse.ArgumentParser(
+            epilog=epilog, add_help=True,
+            formatter_class=argparse.RawDescriptionHelpFormatter)
+
+        # Option parsing general notes
+        # 1.  I can't use store_true. See comments in __init__() for why.
+        #     Instead I use (action='store_const', const=True)
+
+        # Options for the script itself
+        parser.add_argument('--debug', '-d', action='store_const', const=True)
+        parser.add_argument(
+            '--configfile', '-c', type=exresolvepath,
+            help="The path to a config file")
+        parser.add_argument(
+            '--showconfig', action='store_true',
+            help="If passed, gather the arguments from the command line and config files, print the configuration, but exit before performing any action")
+
+        # Options for all subcommands dealing with the YAML template
+        templateopts = argparse.ArgumentParser(add_help=False)
+        templateopts.add_argument('--arm-template', type=exresolvepath)
+
+        # Options for all subcommands dealing with builder VM credentials
+        buildvmcredopts = argparse.ArgumentParser(add_help=False)
+        buildvmcredopts.add_argument('--builder-vm-admin-username')
+        buildvmcredopts.add_argument('--builder-vm-admin-password')
+
+        # Options for all subcommands dealing with Azure credentials
+        azurecredopts = argparse.ArgumentParser(add_help=False)
+        azurecredopts.add_argument('--service-principal-id')
+        azurecredopts.add_argument('--service-principal-key')
+        azurecredopts.add_argument('--tenant')
+        azurecredopts.add_argument('--subscription-id')
+
+        # Options for all subcommands dealing with the Azure resource group
+        azurergopts = argparse.ArgumentParser(add_help=False)
+        azurergopts.add_argument('--resource-group-name')
+
+        # Options for the deploy subcommand
+        deployopts = argparse.ArgumentParser(add_help=False)
+        deployopts.add_argument('--deployment-name')
+        deployopts.add_argument('--resource-group-location')
+        deployopts.add_argument('--storage-account-name')
+        deployopts.add_argument('--builder-vm-size')
+
+        # Options for the connect subcommand
+        connectopts = argparse.ArgumentParser(add_help=False)
+        connectopts.add_argument(
+            '--builder-vm-host',
+            help='The remote host to connect to')
+
+        # Configure subcommands
+        subparsers = parser.add_subparsers(dest="action")
+        subparsers.add_parser(
+            'convertyaml', parents=[templateopts],
+            help='Convert the YAML template to JSON')
+        subparsers.add_parser(
+            'deploy',
+            parents=[templateopts, buildvmcredopts, azurecredopts, azurergopts, deployopts],
+            help='Deploy the ARM template to Azure')
+        subparsers.add_parser(
+            'delete', parents=[azurecredopts, azurergopts],
+            help='Delete an Azure Resource Group')
+        subparsers.add_parser(
+            'testgroup', parents=[azurecredopts],
+            help='Check if the resource group has been deployed')
+        subparsers.add_parser(
+            'connect', parents=[buildvmcredopts, connectopts],
+            help='Connect to the cloud builder VM over RDP')
+
+        return parser.parse_args()
+
+    def parseconfig(self, configs=[]):
+        allconfigs = [cfg for cfg in [self.mastercfg, self.usercfg] + configs if cfg]
+        configdict = {}
+        config = configparser.ConfigParser()
+        config.read(allconfigs)
+        for k in config['DEFAULT'].keys():
+            configdict[k] = config['DEFAULT'].get(k)
+
+        # Munge dictionary values to be the right types
+        def setboolval(dictionary, key):
+            if key in dictionary.keys():
+                boolval = False
+                if dictionary[key] in ['true', 'True', 'yes', 'Yes', 1]:
+                    boolval = True
+                dictionary[key] = boolval
+
+        setboolval(configdict, 'debug')
+
+        return configdict
+
+    def apply_runtime_defaults(self):
+        """Apply defaults to some arguments
+
+        Most argument defaults can be handled by setting them in the config
+        file. However, some defaults are generated at runtime; these are set
+        here.
+        """
+
+        def setifempty(obj, name, default):
+            if not getattr(obj, name):
+                setattr(obj, name, default)
+
+        setifempty(self, 'builder_vm_admin_password', genpass())
+        setifempty(self, 'arm_template', self.defaulttempl)
+
+        datestamp = datetime.datetime.now().strftime('%Y-%d-%m-%H-%M-%S')
+        setifempty(self, 'deployment_name', f'wintriallab-{datestamp}')
+
+    def check_required_params(self):
+        """Check the required parameters
+
+        Ensure that all required parameters are passed either on the command
+        line or as configuration values.configuration.
+
+        (We can't just use required=True on .add_argument() because we also
+        have to check whether it was in the config file(s).)
+        """
+
+        if not hasattr(self, 'action'):
+            raise Exception("You must pass an 'action' parameter")
+        elif self.action == 'convertyaml':
+            required = ['arm_template']
+        elif self.action == 'testgroup':
+            required = [
+                'service_principal_id', 'service_principal_key',
+                'tenant', 'subscription_id',
+                'resource_group_name']
+        elif self.action == 'deploy':
+            required = [
+                'arm_template',
+                'service_principal_id', 'service_principal_key',
+                'tenant', 'subscription_id',
+                'resource_group_name',
+                'resource_group_location',
+                'storage_account_name',
+                'builder_vm_admin_username', 'builder_vm_admin_password',
+                'builder_vm_size', 'deployment_name']
+        elif self.action == 'delete':
+            required = [
+                'service_principal_id', 'service_principal_key',
+                'tenant', 'subscription_id',
+                'resource_group_name']
+        elif self.action == 'connect':
+            required = [
+                'builder_vm_admin_username', 'builder_vm_admin_password',
+                'builder_vm_host']
+        else:
+            raise Exception(f"I don't know how to handle an action of '{self.action}'")
+
+        for parameter in required:
+            if not hasattr(self, parameter):
+                raise Exception(f"Missing parameter '{parameter}' was not passed on the command line or set as a configuration value")
 
 
 def main(*args, **kwargs):
-    parsed = parseargs(args, kwargs)
-    if parsed.debug:
+    config = ProcessedDeployConfig(args, kwargs)
+
+    if config.showconfig:
+        print("PROCESSED CONFIGURATION: ")
+        for k, v in config.__dict__.items():
+            print(f"{k} = {v}")
+        return 0
+
+    if config.debug:
         sys.excepthook = idb_excepthook
         log.setLevel(logging.DEBUG)
 
-    with open(parsed.arm_template) as tf:
+    with open(config.arm_template) as tf:
         template = yaml.load(tf)
 
     # First, handle actions that do NOT require talking to the API (fast)
-    if parsed.action == 'convertyaml':
-        jsonfile = parsed.arm_template.replace('.yaml', '.json')
+    if config.action == 'convertyaml':
+        jsonfile = config.arm_template.replace('.yaml', '.json')
         with open(jsonfile, 'w+') as jtf:
             jtf.write(json.dumps(template, indent=2))
         print("Converted template from YAML to JSON and saved to {jsontempl}")
@@ -237,20 +428,29 @@ def main(*args, **kwargs):
     # Now work with actions that do require talking to the API (blocking/slower)
     resource = ResourceManagementClient(
         ServicePrincipalCredentials(
-            client_id=parsed.service_principal_id,
-            secret=parsed.service_principal_key,
-            tenant=tname2tid(parsed.tenant)),
-        parsed.subscription_id)
+            client_id=config.service_principal_id,
+            secret=config.service_principal_key,
+            tenant=tname2tid(config.tenant)),
+        config.subscription_id)
 
-    if parsed.action == 'delete':
-        resource.resource_groups.delete(parsed.group_name).wait()
-        print(f"Successfully deleted the {parsed.group_name} resource group")
-
-    elif parsed.action == 'deploy':
+    if config.action == 'testgroup':
+        try:
+            resource.resource_groups.get(config.resource_group_name)
+            print(f"YES, the resource group '{config.resource_group_name}'' is deployed and costing you $$$")
+        except CloudError as exp:
+            if exp.status_code == 404:
+                print(f"NO, the resource group '{config.resource_group_name}'' is not present")
+            else:
+                raise exp
+    elif config.action == 'delete':
+        resource.resource_groups.delete(config.resource_group_name).wait()
+        print(f"Successfully deleted the {parsed.resource_group_name} resource group")
+    elif config.action == 'deploy':
         result = deploytempl(
-            resource, parsed.group_name, parsed.group_location,
-            parsed.storage_account_name, parsed.builder_vm_admin_password,
-            parsed.builder_vm_size, template, parsed.deployment_name)
+            resource, config.resource_group_name, config.resource_group_location,
+            config.storage_account_name,
+            config.builder_vm_admin_username, config.builder_vm_admin_password,
+            config.builder_vm_size, template, config.deployment_name)
         msg = "Deployment completed. Outputs:"
         for k, v in result.properties.outputs.items():
             msg += f"\n- {k} = {str(v['value'])}"
