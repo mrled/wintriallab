a7a8f23d7729f243c653c06ce3a258f123980f9d
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-08-25 11:51:24 -0500

    Use DSC to handle more configuration
    Use DSC to perform the following:
    - Install Chocolatey
    - Install Git
    - Install Packer
    - Checkout the WinTrialLab repo
    - Install the Caryatid Packer plugin
    
    Also simplify deployInit.ps1 invocation somewhat in cloudbuilder.yaml

--- azure/deployInit.ps1
+++ azure/deployInit.ps1
@@ -11,11 +11,31 @@ The name to use for the "source" of the event log entries we create. Arbitrary s
 The URL for the "magic" script. This script is run and immediately executed. Useful for arbitrary customization.
 #>
 [CmdletBinding()] Param(
-    [Parameter(Mandatory)] [string] $winTrialLabDir,
+    [Parameter(Mandatory)] [string] $wtlRepoUri,
+    [Parameter(Mandatory)] [string] $wtlRepoBranch,
+    [string] $wtlCheckoutDir = "${env:SystemDrive}\WinTrialLab",
+
+    [string] $chocoInstallDir = (Join-Path -Path ${env:ProgramData} -ChildPath "Chocolatey"),
+
+    [string] $caryatidInstallDir = (Join-Path -Path "${env:AppData}\packer.d" -ChildPath 'plugins'),
+    [string] $caryatidPluginFilename = 'packer-post-processor-caryatid.exe',
+    [string] $caryatidInstallPath = (Join-Path -Path $caryatidInstallDir -ChildPath $caryatidPluginFilename),
+    [string] $caryatidGitHubLatestReleaseEndpoint = 'https://api.github.com/repos/mrled/caryatid/releases/latest',
+    [string] $caryatidAssetRegex = '^caryatid_windows_amd64_.*\.zip$',
+
     [string] $eventLogName = "WinTrialLab",
-    [string] $eventLogSource = "WinTrialLab-azure-deployInit.ps1",
-    [string] $magicUrl = "https://raw.githubusercontent.com/mrled/dhd/master/opt/powershell/magic.ps1"
+    [string] $eventLogSource = "WinTrialLab-azure-deployInit.ps1"
 )
+
+<#
+Development notes:
+
+Do *not* use $PSScriptRoot to reference other files in the repository. This script is downloaded and run to clone the repo. Once the repo is cloned, reference files from $wtlCheckoutDir
+#>
+
+$ErrorActionPreference = "Stop"
+
+## Helper functions
 
 <#
 .synopsis
@@ -89,61 +109,131 @@ function Invoke-PathExecutable {
     }
 }
 
-<#
-.description
-Set the execution policy to unrestricted for both 32 and 64 bit processes
-#>
-function Set-ExecPolUnrestricted {
-    [CmdletBinding()] Param()
-    $sepScope = if (Test-AdministratorRole) { "LocalMachine" } else { "CurrentUser" }
-    Set-ExecutionPolicy Unrestricted -Scope $sepScope -Force
-    if ([Environment]::Is64BitProcess) {
-        Start-Job -ScriptBlock { Set-ExecutionPolicy Unrestricted -Scope $sepScpope -Force } -RunAs32
-    }
-    Write-EventLogWrapper -message "ExecutionPolicy now set to restricted for scope '$sepScope'"
+function New-TemporaryDirectory {
+    $newTempDirPath = ""
+    do {
+        $newTempDirPath = Join-Path $env:TEMP (New-Guid | Select-Object -ExpandProperty Guid)
+    } while (Test-Path -Path $newTempDirPath)
+    New-Item -ItemType Directory -Path $newTempDirPath
 }
 
-function Install-Chocolatey {
-    Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
-    Update-EnvironmentPath
-    $chocoPath = Get-Command choco.exe | Select-Object -ExpandProperty Source
-    Invoke-PathExecutable "choco.exe feature enable --name=allowGlobalConfirmation --yes"
-    Write-EventLogWrapper -message "Chocolatey is now installed to '$chocoPath'"
-}
+## DSC Configuration
 
-function Install-ChocolateyPackage {
-    [CmdletBinding()] Param(
-        [Parameter(Mandatory=$true)] [String[]] $packageName
+[DSCLocalConfigurationManager()]
+Configuration DSConfigure-LocalConfigurationManager {
+    param(
+        [string[]] $computerName = "localhost"
     )
-    Invoke-PathExecutable "choco.exe install $packageName"
+    Node $computerName {
+        Settings {
+            RebootNodeIfNeeded = $true
+            ActionAfterReboot = 'ContinueConfiguration'
+        }
+    }
 }
 
-Write-EventLogWrapper -message "Initializing the WinTrialLab cloud builder deployment..."
-Set-ExecPolUnrestricted
+Configuration DSConfigure-WinTrialBuilder {
+    param(
+        [string[]] $computerName = $env:COMPUTERNAME
+    )
 
-Install-Chocolatey
-Install-ChocolateyPackage -packageName @('packer', 'git')
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName xHyper-V
 
-# Get my dhd repository and configure Powershell to use my $profile etc. See the script for details.
-# When the cloudbuilder is better tested, I won't need to connect over RDP at all.
-# Since this is just my custom Powershell profile etc, I'd like to remove this eventually.
-Invoke-WebRequest -UseBasicParsing $magicUrl | Invoke-Expression
+    # TODO: Do I have to chain all these with DependsOn? Can I have an array in DependsOn or anything?
 
-# Hyper-V and DSC stuff... refactor later
-Install-PackageProvider -Name NuGet -Force
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name xHyper-V
+    Node $computerName {
 
-# Initialize DSC configuration
-. $winTrialLabDir\azure\dscConfig.ps1
-$dscWorkDirBase = mkdir -Force "$winTrialLabDir\DscWorkDir" | Select-Object -ExpandProperty FullName
+        WindowsFeature "Hyper-V" {
+            Ensure = "Present"
+            Name = "Hyper-V"
+        }
+        WindowsFeature "Hyper-V-Tools" {
+            Ensure = "Present"
+            Name = "Hyper-V-Tools"
+            DependsOn = "[WindowsFeature]Hyper-V"
+        }
+        WindowsFeature "Hyper-V-Powershell" {
+            Ensure = "Present"
+            Name = "Hyper-V-Powershell"
+            DependsOn = "[WindowsFeature]Hyper-V"
+        }
+        # This may not be necessary?
+        xVMHost "VmHostConfiguration" {
+            IsSingleInstance = 'Yes'
+            DependsOn = "[WindowsFeature]Hyper-V"
+        }
 
-# Configure the Local Configuration Manager first
-$lcmWorkDir = Join-Path $dscWorkDirBase "LocalConfigurationManager"
-DSConfigure-LocalConfigurationManager -OutputPath $lcmWorkDir | Write-EventLogWrapper
-Set-DscLocalConfigurationManager -Path $lcmWorkDir | Write-EventLogWrapper
+        cChocoInstaller "InstallChoco" {
+            InstallDir = $chocoInstallDir
+        }
+        cChocoPackageInstaller "ChocoInstallGit" {
+            Name      = 'git'
+            Ensure    = 'Present'
+            DependsOn = '[cChocoInstaller]InstallChoco'
+        }
+        cChocoPackageInstaller "ChocoInstallPacker" {
+            Name      = 'packer'
+            Ensure    = 'Present'
+            DependsOn = '[cChocoPackageInstaller]ChocoInstallGit'
+        }
+        Script "CheckoutWinTrialLab" {
+            GetScript = {}
+            TestScript = {
+                Test-Path -Path "$wtlCheckoutDir\.git"
+            }
+            SetScript = {
+                Invoke-PathExecutable "git.exe clone '$wtlRepoUri' --branch '$wtlRepoBranch' '$wtlCheckoutDir'"
+            }
+            DependsOn = "[cChocoPackageInstaller]ChocoInstallPacker"
+        }
 
-# Now run the WinTrialLab DSC configuration
-$wtlWorkDir = Join-Path $dscWorkDirBase "WinTrialLab"
-DSConfigure-WinTrialBuilder -OutputPath $wtlWorkDir | Write-EventLogWrapper
-Start-DscConfiguration -Path $wtlWorkDir | Write-EventLogWrapper
+        Script "InstallCaryatidPackerPlugin" {
+            GetScript = {}
+            TestScript = {
+                Test-Path -Path $caryatidInstallPath
+            }
+            SetScript = {
+                $asset = Invoke-RestMethod -Uri $caryatidGitHubLatestReleaseEndpoint | Select-Object -ExpandProperty assets | Where-Object -Property "name" -match $caryatidAssetRegex
+                $filename = $asset.browser_download_url -split '/' | Select-Object -Last 1
+                $downloadDir = New-TemporaryDirectory | Select-Object -ExpandProperty FullName
+                $downloadPath = Join-Path -Path $downloadDir -ChildPath $filename
+                New-Item -Type Directory -Force -Path $caryatidInstallDir
+                Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $downloadPath
+                Expand-Archive -Path $downloadPath -DestinationPath $downloadDir
+                $caryatidExe = Get-ChildItem -Recurse -File -Path $downloadDir -Include
+                Move-Item -Path $caryatidExe -Destination $caryatidInstallPath
+            }
+            DependsOn = "[Script]CheckoutWinTrialLab"
+        }
+
+        # Script "RunPacker" {
+        #     GetScript = {}
+        #     TestScript = {}
+        #     SetScript = {}
+        # }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') {
+    Write-EventLogWrapper -message "Initializing the WinTrialLab cloud builder deployment..."
+    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Machine
+
+    Install-PackageProvider -Name NuGet -Force
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-Module -Name xHyper-V, cChoco
+
+    # Initialize DSC configuration
+    $dscWorkDirBase = New-TemporaryDirectory | Select-Object -ExpandProperty FullName
+    Write-EventLogWrapper -message "Using '$dscWorkDirBase' for DSC configurations"
+
+    # Configure the Local Configuration Manager first
+    $lcmWorkDir = Join-Path $dscWorkDirBase "LocalConfigurationManager"
+    DSConfigure-LocalConfigurationManager -OutputPath $lcmWorkDir | Write-EventLogWrapper
+    Set-DscLocalConfigurationManager -Path $lcmWorkDir | Write-EventLogWrapper
+
+    # Now run the WinTrialLab DSC configuration
+    $wtlWorkDir = Join-Path $dscWorkDirBase "WinTrialLab"
+    DSConfigure-WinTrialBuilder -OutputPath $wtlWorkDir | Write-EventLogWrapper
+    Start-DscConfiguration -Path $wtlWorkDir | Write-EventLogWrapper
+}
