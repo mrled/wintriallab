12883fc7bdfe44fe1dbecd9a1fd48cf093565a2b
Micah R Ledbetter <mledbetter@certicasolutions.com>
2017-11-29 11:10:15 -0600

    Remove old freyja provisioner

--- scripts/freyja-provisioner.ps1
+++
@@ -1,8 +0,0 @@
-<#
-.description
-Provision the 'Freyja' boxes, which I use for DLP
-#>
-
-$ErrorActionPreference = "Stop"
-
-Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/mrled/dhd/master/opt/powershell/magic.ps1 | Invoke-Expression
